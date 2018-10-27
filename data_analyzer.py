c44ccc14775478b9fd37ba57e138d6da6eb48e66
Jon <jonathan.povish19@houghton.edu>
2018-10-27 12:05:55 -0400

    This is a fix to analyzer

--- data_analyzer.py
+++ data_analyzer.py
@@ -3,11 +3,26 @@ sys.path.insert(0, "./Quantum")
 sys.path.insert(0, "./Classical")
 sys.path.insert(0, "./Tensorflow")
 import googlemaps
-import sklearn
-from sklearn import linear_model
+import data_formation
+from sklearn import linear_model, decision_tree_classification, decision_tree_regression
 from kmeans_hybrid import QKMeans
+import requests
 
 gmaps = googlemaps.Client(key='AIzaSyDlPGwvHDrnUh7cugYNIngmJPmZAWAN_VY')
+
+
+def getLat():   
+    address = "1600 Amphitheatre Parkway, Mountain View, CA"
+    api_key = "AIzaSyDlPGwvHDrnUh7cugYNIngmJPmZAWAN_VY"
+    api_response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(address, api_key))
+    api_response_dict = api_response.json()
+
+    if api_response_dict['status'] == 'OK':
+        latitude = api_response_dict['results'][0]['geometry']['location']['lat']
+        longitude = api_response_dict['results'][0]['geometry']['location']['lng']
+        print ('Latitude:' + str(latitude))
+        print ('Longitude:' + str(longitude))
+
 
 # returns a complete quantum kmeans model
 def qkmeans_analysis(X, y, k):
@@ -29,8 +44,13 @@ def decision_tree_regression(X, y):
     return dtr
 
 def lineReg(X,y):
-    regr = linear_model.LinearRegression()
+    regr = sklearn.linear_model.LinearRegression()
  # Train the model using the training sets
     regr.fit(X_train, Y_train)
     return(regr)
 
+    
+CrimeData = data_formation.import_dataframe("CrimeData.csv")
+#CrimeLoc = CrimeData[]
+    
+    
