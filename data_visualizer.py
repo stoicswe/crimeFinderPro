3072e2c292e62154a61b7cbbb4835baed757639b
Jon <jonathan.povish19@houghton.edu>
2018-10-27 15:56:31 -0400

    Fixing error

--- data_visualizer.py
+++ data_visualizer.py
@@ -138,7 +138,7 @@ def plotDays():
     plt.title('Programming language usage')
     plt.show()
 
-CrimeData = import_dataframe('‪./Datasets/CrimeData_2012.csv')
+CrimeData = pd.read_csv('‪./Datasets/CrimeData_2012.csv')
 plotCrimes()
 plotDays()
 plotHours()
