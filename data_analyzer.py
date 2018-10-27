ab1ce3fe61ad98bb47814be87f70aabb01ea065d
Jon <jonathan.povish19@houghton.edu>
2018-10-27 11:43:01 -0400

    Gitasdfasdawer

--- data_analyzer.py
+++ data_analyzer.py
@@ -4,6 +4,7 @@ sys.path.insert(0, "./Classical")
 sys.path.insert(0, "./Tensorflow")
 import googlemaps
 import sklearn
+from sklearn import linear_model
 from kmeans_hybrid import QKMeans
 
 gmaps = googlemaps.Client(key='AIzaSyDlPGwvHDrnUh7cugYNIngmJPmZAWAN_VY')
@@ -16,8 +17,6 @@ def qkmeans_analysis(X, y, k):
     print("ClusterCentroids:")
     print(qkm.means)
     return qkm
-<<<<<<< HEAD
-=======
 
 def decision_tree_classification(X, y):
     dtc = sklearn.tree.DecisionTreeClassifier()
@@ -28,4 +27,10 @@ def decision_tree_regression(X, y):
     dtr = sklearn.tree.DecisionTreeRegressor()
     dtr = dtr.fit(X,y)
     return dtr
->>>>>>> e36fa7d3654b58b07595d8b271acd2c6a50d1a2c
+
+def lineReg(X,y):
+    regr = linear_model.LinearRegression()
+ # Train the model using the training sets
+    regr.fit(X_train, Y_train)
+    return(regr)
+
