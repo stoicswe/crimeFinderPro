c632efb7cd665ff75956d5fa5ce76c7969790d68
Jon <jonathan.povish19@houghton.edu>
2018-10-27 03:55:20 -0400

    Just Finished for tonight, Got a working pi chart, need to tidy it up

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 from datetime import datetime
-
+import data_formation
 ''' Below is an example of a simple pie chart 
 labels = 'Python', 'C++', 'Ruby', 'Java'
 #Replace this with labels of crime
@@ -17,22 +18,39 @@ plt.pie(sizes, explode=explode, labels=labels, colors=colors,
 plt.axis('equal')
 plt.show()
 '''
+
+
 #Replace Vals With #of crime
-def plotCrimes()
-    vals = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]])
+def plotCrimes():
+    #Working Currently Right here
+    crimeColumn = CrimeData['StatuteCrimeCategory'].tolist()
+    #Below line change the 'time' to time crime it is'
+    sexColumn = CrimeData['StatuteCategory'].tolist()    
+    crimes = list(set(crimeColumn))
+    vals = []
+    for x in range(len(crimes)):
+        vals.append([0,0])
+    for y in range(len(crimeColumn)):
+        if(sexColumn[y] == 'M'):
+            vals[crimes.index(crimeColumn[y])][0] += 1
+        else:
+            vals[crimes.index(crimeColumn[y])][1] += 1
+        
+    vals = np.asarray(vals)
+    #vals = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]])
     fig, ax = plt.subplots()
-    ax.pie(vals.flatten(), radius=1.2,
-           colors=plt.rcParams["axes.prop_cycle"].by_key()["color"][:vals.shape[1]])
+    print(vals)
+    ax.pie(vals.flatten(), radius=1.2, colors=plt.rcParams["axes.prop_cycle"].by_key()["color"][:vals.shape[1]])
     ax.pie(vals.sum(axis=1), radius=1)
     ax.set(aspect="equal", title='Pie plot with `ax.pie`')
     plt.show()
-    clf()
-    close()
+    #clf()
+    #close()
 
 
 def plotHours():
     #Below line change the 'crime' to whatever crime it is'
-    crimeColumn = CrimeData['Crime'].toList()
+    crimeColumn = CrimeData['StatuteCrimeCategory'].tolist()
     #Below line change the 'time' to time crime it is'
     dateColumn = CrimeData['Time'].toList()
 
@@ -60,7 +78,7 @@ def plotHours():
 
 def plotDays():
     #Below line change the 'crime' to whatever crime it is'
-    crimeColumn = CrimeData['Crime'].toList()
+    crimeColumn = CrimeData['Statute_Category'].toList()
     #Below line change the 'time' to where the full time crime it is'
     dateColumn = CrimeData['Time'].toList()
     
@@ -74,8 +92,18 @@ def plotDays():
     for z in range(len(crimes)):
         d = datetime.strptime(dateColumn[z].split(",")[0], "%m/%d/%Y")
 
-        totalTime.append(d.weeday())
+        totalTime.append(d.weekday())
         CrimeTimeSplit[crimesType.index(crimes[x])].append(d.weekday())
         
+    objects = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    performance = []
+    for x in range(len(object)):
+        performance.append(totalTime.count(x))
+    #performance = [10,8,6,4,2,1]
+    plt.barh(y_pos, performance, align='center', alpha=0.5)
+    plt.yticks(y_pos, objects)
+    plt.xlabel('Usage')
+    plt.title('Programming language usage')
+    plt.show()
 
-
+CrimeData = data_formation.import_dataframe("CrimeData.csv")
